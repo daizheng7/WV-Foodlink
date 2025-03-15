8a1a6311bdfabb258a63ff58ec1ead90dbb4c96c
Davide Giacomo Zoppolato <109225286+daizheng7@users.noreply.github.com>
2025-03-15 09:57:33 -0400

    feat(landscape): fixed links and icons.

--- arcgis-react-app/src/components/WestVirginiaFoodLandscape.js
+++ arcgis-react-app/src/components/WestVirginiaFoodLandscape.js
@@ -1,20 +1,20 @@
 import React from 'react';
 import { Card, CardContent, CardActionArea, Typography, Tooltip } from '@mui/material';
-import { AccessAlarm, Info, Restaurant, LocalMall, Agriculture, Home, AccountBalance, Public, LocalDining, Lightbulb, Diversity3, LightbulbCircle, ZoomIn } from '@mui/icons-material';
+import { AccessAlarm, Info, Restaurant, LocalMall, Agriculture, Home, AccountBalance, Public, LocalDining, Lightbulb, Diversity3, LightbulbCircle, ZoomIn, CreditCard, School, Storefront, AgricultureOutlined, NaturePeople } from '@mui/icons-material';
 
 const categories = [
-  { title: 'SNAP + WIC', icon: <AccessAlarm sx={{ fontSize: 160, color: '#354F5B' }} />, description: 'Covers nutrition assistance programs supporting food access.', url: 'https://www.arcgis.com/apps/dashboards/5095c3fd9b0c4934be43bc8f65c93b36' },
-  { title: 'SNAP-ED', icon: <Info sx={{ fontSize: 160, color: '#39897E' }} />, description: 'Education programs to improve nutrition and policy awareness.', url: 'https://www.arcgis.com/apps/dashboards/2100f46c379b49ba8d5d4184c68d0ab0' },
-  { title: 'Congregate Meals', icon: <Restaurant sx={{ fontSize: 160, color: '#B1B5AB' }} />, description: 'Includes school meal programs, daycare food services, and senior meals.', url: 'https://www.arcgis.com/apps/dashboards/28258179da3a4fd0b1dcd0a053d402ec' },
-  { title: 'Charitable Food', icon: <LocalMall sx={{ fontSize: 160, color: '#C84C23' }} />, description: 'Details food pantries, soup kitchens, and school backpack programs.', url: 'https://www.arcgis.com/apps/dashboards/60c2dc75756c485f9d135d14826464f4' },
-  { title: 'Farmers Markets', icon: <Agriculture sx={{ fontSize: 160, color: '#8B0000' }} />, description: 'Information on local farmer-to-consumer markets and nutrition incentives.', url: 'https://www.arcgis.com/apps/dashboards/5095c3fd9b0c4934be43bc8f65c93b36' },
-  { title: 'Agricultural Data', icon: <Home sx={{ fontSize: 160, color: '#445525' }} />, description: 'Insights into farming practices, land use, and expenditures.', url: 'https://www.arcgis.com/apps/dashboards/28258179da3a4fd0b1dcd0a053d402ec' },
-  { title: 'Self-Provisioning', icon: <Info sx={{ fontSize: 160, color: '#D4D2C2' }} />, description: 'Gardening and hunting data related to food self-sufficiency.', url: 'https://www.arcgis.com/apps/dashboards/a2bc19f10d9647c39992283e9ec4d851' },
-  { title: 'Political Participation', icon: <AccountBalance sx={{ fontSize: 160, color: '#39897E' }} />, description: 'Maps and data related to food policy engagement.', url: 'https://www.arcgis.com/apps/dashboards/f44ec87fbabf4697959012640dc2d29f' },
-  { title: 'County Summary', icon: <Public sx={{ fontSize: 160, color: '#354F5B' }} />, description: 'Data related to county-level food landscape.', url: 'https://www.arcgis.com/apps/dashboards/f44ec87fbabf4697959012640dc2d29f' },
-  { title: 'Find Food', icon: <ZoomIn sx={{ fontSize: 160, color: '#C84C23' }} />, description: 'App to help you find fresh food based on your location.', url: 'https://www.arcgis.com/apps/dashboards/f44ec87fbabf4697959012640dc2d29f' },
-  { title: 'Organize', icon: <Diversity3 sx={{ fontSize: 160, color: '#B1B5AB' }} />, description: 'Discove', url: 'https://www.arcgis.com/apps/dashboards/f44ec87fbabf4697959012640dc2d29f' },
-  { title: 'Learn', icon: <LightbulbCircle sx={{ fontSize: 160, color: '#8B0000' }} />, description: 'Discove', url: 'https://www.arcgis.com/apps/dashboards/f44ec87fbabf4697959012640dc2d29f' }
+  { title: 'SNAP + WIC', icon: <CreditCard sx={{ fontSize: 160, color: '#354F5B' }} />, description: 'Enrollment in Cash-Equivalent Public Nutrition Assistance Programs.', url: 'https://www.arcgis.com/apps/dashboards/f44ec87fbabf4697959012640dc2d29f' },
+  { title: 'SNAP-ED', icon: <School sx={{ fontSize: 160, color: '#39897E' }} />, description: 'Food Policy, Systems and Environmental Change interventions for Healthy Food Access Programming.', url: 'https://wvu.maps.arcgis.com/apps/dashboards/a2bc19f10d9647c39992283e9ec4d851' },
+  { title: 'Congregate Meals', icon: <Restaurant sx={{ fontSize: 160, color: '#B1B5AB' }} />, description: 'Public Meal programs in Schools, Daycares and Senior Centers.', url: 'https://wvu.maps.arcgis.com/apps/instant/portfolio/index.html?appid=b442bf3a130248938d3f4323840fe50e' },
+  { title: 'Charitable Food', icon: <LocalMall sx={{ fontSize: 160, color: '#C84C23' }} />, description: 'Details food pantries, soup kitchens, and school backpack programs.', url: 'https://wvu.maps.arcgis.com/apps/dashboards/783922e1a38646bda92e8ddfbb37961b' },
+  { title: 'Farmers Markets', icon: <Storefront sx={{ fontSize: 160, color: '#8B0000' }} />, description: 'Information on local farmer-to-consumer markets and nutrition incentives.', url: 'https://www.arcgis.com/apps/dashboards/5095c3fd9b0c4934be43bc8f65c93b36' },
+  { title: 'Agricultural Data', icon: <Agriculture sx={{ fontSize: 160, color: '#445525' }} />, description: 'Insights into farming practices, land use, and expenditures.', url: 'https://www.arcgis.com/apps/dashboards/2100f46c379b49ba8d5d4184c68d0ab0'},
+  { title: 'Self-Provisioning', icon: <NaturePeople sx={{ fontSize: 160, color: '#D4D2C2' }} />, description: 'Gardening and hunting data related to food self-sufficiency.', url: 'https://www.arcgis.com/apps/dashboards/60c2dc75756c485f9d135d14826464f4' },
+  { title: 'Political Participation', icon: <AccountBalance sx={{ fontSize: 160, color: '#39897E' }} />, description: 'Voter registration and engagement.', url: 'https://www.arcgis.com/apps/dashboards/28258179da3a4fd0b1dcd0a053d402ec' },
+  { title: 'County Summary', icon: <Public sx={{ fontSize: 160, color: '#354F5B' }} />, description: 'Data related to county-level food landscape.', url: '/county' },
+  { title: 'Find Food', icon: <ZoomIn sx={{ fontSize: 160, color: '#C84C23' }} />, description: 'App to help you find fresh food based on your location.', url: '/food' },
+  { title: 'Organize', icon: <Diversity3 sx={{ fontSize: 160, color: '#B1B5AB' }} />, description: 'Discover how to organize for the right to food in your community.', url: 'https://foodlink.wvu.edu/pages/organize-1' },
+  { title: 'Learn', icon: <LightbulbCircle sx={{ fontSize: 160, color: '#8B0000' }} />, description: 'Learn how to create just and sustainable communities.', url: 'https://resilientcommunities.wvu.edu/' }
 ];
 
 const WestVirginiaFoodLandscape = () => {
